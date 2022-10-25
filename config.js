b641d45424d56f285023d721adf64ebeefe34b4c
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-10-25 09:29:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6282146218274']
+global.ownernomer = "6282146218274"
+global.premium = ['6282146218274']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'Rɪᴇʟʟ X Wᴀʜʏᴜ Aɴɪᴍᴇᴋ'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -66,9 +66,9 @@ global.mygit = 'https://github.com/nazedev'
 global.myyt = 'https://youtube.com/c/Nazedev'
 global.myytv = 'https://youtu.be/ey7qLuRBm5Y'
 global.mygc = "https://chat.whatsapp.com/Khpnk6svwIhCIvx91Xi0Vf"
-global.botname = 'Naze Dev'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE'
+global.botname = 'ᴋʜᴜʀᴜsʜɪ ʙᴏᴛ'
+global.akulaku = 'Bot By Rɪᴇʟʟ X Wᴀʜʏᴜ Aɴɪᴍᴇᴋ'
+global.ytname = 'YT RIELLBOTZ OFFC'
 
 
 let file = require.resolve(__filename)
